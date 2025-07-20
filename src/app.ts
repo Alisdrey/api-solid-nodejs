ec168887cb2c09cd094dce1f02263a45ad3681f2
Alisson Andrey <alissoncarmo2010@hotmail.com>
2025-07-20 15:57:38 -0300

    feature: Integração com Prisma ORM - Fundamentos do Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Alisson Andrey',
+    email: 'alisson@email.com.br',
+  },
+})
+
+// ORM - Object Relational Mapper
